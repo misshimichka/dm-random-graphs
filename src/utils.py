bf3c50744ef01ca89b4b05f7381af9f9e61b0926
Taisiia Chegodaeva <chegodaeva.stacy@gmail.com>
2025-05-16 09:10:27 +0300

    Fix codestyle in utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,24 @@
+"""
+Functions to build graphs based on samples from some distribution.
+"""
+
 from typing import List
 
 
 def build_knn_graph(vertices: List[float], k: int) -> List[List[int]]:
-    k = min(k, len(vertices))
+    """
+    Function builds KNN-graph.
+
+    vertices: list of float numbers (= samples of some distribution).
+    k: number of neighbours.
+    """
+    k = min(k, len(vertices) - 1)
 
     neighbours = [[] for _ in range(len(vertices))]
-    for i in range(len(vertices)):
-        for j in range(i + 1, len(vertices)):
-            neighbours[i].append((j, abs(vertices[i] - vertices[j])))
-            neighbours[j].append((i, abs(vertices[i] - vertices[j])))
+    for i, value_i in enumerate(vertices):
+        for j, value_j in enumerate(vertices[i + 1 :]):
+            neighbours[i].append((j, abs(value_i - value_j)))
+            neighbours[j].append((i, abs(value_i - value_j)))
         neighbours[i].sort(key=lambda x: x[1])
 
     graph = [[0 for _ in range(len(vertices))] for _ in range(len(vertices))]
@@ -20,10 +30,16 @@ def build_knn_graph(vertices: List[float], k: int) -> List[List[int]]:
 
 
 def build_dist_graph(vertices: List[float], dist: float) -> List[List[int]]:
+    """
+    Function builds distance graph.
+
+    vertices: list of float numbers (= samples of some distribution).
+    dist: distance threshold value.
+    """
     graph = [[0 for _ in range(len(vertices))] for _ in range(len(vertices))]
-    for i in range(len(vertices)):
-        for j in range(i + 1, len(vertices)):
-            if abs(vertices[i] - vertices[j]) <= dist:
+    for i, value_i in enumerate(vertices):
+        for j, value_j in enumerate(vertices[i + 1 :]):
+            if abs(value_i - value_j) <= dist:
                 graph[i][j] = graph[j][i] = 1
 
     return graph
