2f141c54288437c59e56c92b6d6ddc528f9d59fa
Taisiia Chegodaeva <chegodaeva.stacy@gmail.com>
2025-05-16 11:04:09 +0300

    Fix codestyle

--- src/characteristics.py
+++ src/characteristics.py
@@ -1,12 +1,21 @@
+"""
+Functions to calculate graph characteristics.
+"""
+
 from typing import List
 
 
 def calculate_triangles(graph: List[List[int]]) -> int:
+    """
+    Fuction counts number of triangles in given graph.
+
+    graph: 2-dimentional adjacency matrix.
+    """
     n_triangles = 0
 
-    for i in range(len(graph)):
-        for j in range(i + 1, len(graph)):
-            for k in range(j + 1, len(graph)):
+    for i, _ in enumerate(graph):
+        for j, _ in enumerate(graph[i + 1 :]):
+            for k, _ in enumerate(graph[j + 1 :]):
                 if graph[i][j] and graph[i][k] and graph[j][k]:
                     n_triangles += 1
 
