bd581220433d1c06a9a7fef63c7441e4a1e803dd
Dima <lozhnii@yandex.ru>
2025-05-18 13:55:37 +0300

    Added test for calculations

---
+++ test/test_calculate_size_mis.py
@@ -0,0 +1,43 @@
+"""
+Unit-tests for `calculate_size_mis()` function.
+"""
+
+from src.characteristics import calculate_size_mis
+
+def test_empty_graph():
+    """
+    Test calculation on empty graph.
+    """
+    graph = []
+    expected = 0
+    assert calculate_size_mis(graph) == expected
+    
+def test_one_vertex():
+    """
+    Test calculation on graph with one vertex.
+    """
+    graph = [[0]]
+    expected = 1
+    assert calculate_size_mis(graph) == expected
+
+def test_simple_graph():
+    """
+    Test calculation on simple graph.
+    """
+    graph = [[0, 1, 1, 1],
+             [1, 0, 0, 0],
+             [1, 0, 0, 0],
+             [1, 0, 0, 0]]
+    expected = 3
+    assert calculate_size_mis(graph) == expected
+    
+def test_fully_connected_graph():
+    """
+    Test calculation on fully-connected graph.
+    """
+    graph = [[0, 1, 1, 1]
+             [1, 0, 1, 1]
+             [1, 1, 0, 1]
+             [1, 1, 1, 0]]
+    expected = 1
+    assert calculate_size_mis(graph) == expected
