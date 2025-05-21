b5068ad4359f6b135b2c77a613f36d4a80df5f36
Dima <lozhnii@yandex.ru>
2025-05-21 19:41:02 +0300

    Finished part one

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
 """
-Functions to build graphs based on samples from some distribution.
+Functions to build graphs based on samples from some distribution and generate sets.
 """
-
-from typing import List
+from typing import List, Tuple, Callable, Dict, Union
+from collections import defaultdict
+import numpy as np
 
 
 def build_knn_graph(vertices: List[float], k: int) -> List[List[int]]:
@@ -47,3 +48,77 @@ def build_dist_graph(vertices: List[float], dist: float) -> List[List[int]]:
                 graph[i][j] = graph[j][i] = 1
 
     return graph
+
+
+def generate_H(distribution: int, param: int, n_samples: int = 1000, sample_size: int = 100) -> np.ndarray:
+    """
+    Function generates samples of given distribution.
+    
+    distribution: function to generate sample of distribution
+    param: param for distribution
+    n_samples: number of samples
+    samples_size: size of samples
+    """
+    return np.array([distribution(param, sample_size) for _ in range(n_samples)])
+
+
+def generate_A(
+    H0_samples: np.ndarray,
+    H1_samples: np.ndarray,
+    calculation: int,
+    graph_type: str = "knn",
+    graph_param: int = 5,
+    alpha: float = 0.05
+) -> Tuple:
+    """
+    Function generates set A and counts `power` of this set.
+    
+    H0_samples: samples from first distribution
+    H1_samples: samples from second distribution
+    calculation: function that calculates graph characteristic
+    graph_type: `knn` of 'dist'
+    graph_param: param for graph generation
+    alpha: max prob of error
+    """
+
+    T_H0 = []
+    T_H1 = []
+
+    for sample in H0_samples:
+        if graph_type == "knn":
+            graph = build_knn_graph(sample, graph_param)
+        elif graph_type == "dist":
+            graph = build_dist_graph(sample, graph_param)
+        else:
+            raise ValueError("Unknown graph type")
+        T_H0.append(calculation(graph))
+
+    for sample in H1_samples:
+        if graph_type == "knn":
+            graph = build_knn_graph(sample, graph_param)
+        elif graph_type == "dist":
+            graph = build_dist_graph(sample, graph_param)
+        else:
+            raise ValueError("Unknown graph type")
+        T_H1.append(calculation(graph))
+
+    freq = defaultdict(int)
+    for t in T_H0:
+        freq[t] += 1
+    sorted_T = sorted(freq.keys(), key=lambda x: -freq[x])
+
+    A = set()
+    cur_sum = 0
+    total = len(T_H0)
+
+    for t in sorted_T:
+        if cur_sum / total < 1 - alpha:
+            A.add(t)
+            cur_sum += freq[t]
+        else:
+            break
+
+    power = sum(1 for t in T_H1 if t not in A) / len(T_H1)
+
+    return A, power 
+    
